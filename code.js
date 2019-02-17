31502498e067c7f8a9a31dbe5e07e04b8cceef01
Родион Шергин <beholderrk@gmail.com>
2019-02-18 01:58:07 +0300

    слайды про rx

--- code.js
+++ code.js
@@ -9,3 +9,19 @@ getDataFromNetwork()
   .take(5)
   .map(s => s + " transformed")
   .subscribe(s => console.log(`next => ${s}`))
+
+const searchText$ = fromEvent(this.input.nativeElement).pipe(
+   map(event => event.target.value),
+   startWith(''),
+   debounceTime(400),
+   distinctUntilChanged()
+);
+
+const users$ = searchText$.pipe(
+   switchMap(search => this.loadUsers(search))
+).subscribe();
+
+function loadUsers(search) {
+    const params = new HttpParams().set('search', search);
+    return this.http.get(`/api/course/${coursesId}/users/`, {params});
+}
