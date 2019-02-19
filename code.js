6a5e24a2daa6b7f963dad0d6970137020e0e1fe9
Родион Шергин <beholderrk@gmail.com>
2019-02-19 23:54:50 +0300

    операторы

--- code.js
+++ code.js
@@ -25,3 +25,36 @@ function loadUsers(search) {
     const params = new HttpParams().set('search', search);
     return this.http.get(`/api/course/${coursesId}/users/`, {params});
 }
+
+of(strMessage).pipe(
+    tap(pushToSendQueue),
+    mergeMapTo(receivedMessages.pipe(filter(resp => resp.req_id === reqId))),
+    // если в ответе ошибка, выкидываем exception
+    tap(throwIfError),
+    // повторяем запросы только для ошибок E_REQUEST_THROTTLED
+    retryWhen((errors: Observable<IErrorMessage>) =>
+        errors.pipe(
+            tap(error => {
+                if (error.error !== 'E_REQUEST_THROTTLED') {
+                    throw error;
+                }
+            }),
+            delayWhen<IErrorMessage>(error => timer(error.retry_in))
+        )
+    ),
+);
+
+
+updatesObs.pipe(countConsumers(consumersSub)).subscribe();
+consumersSub.pipe(
+    concatMap((count) => {
+        if (count === 1) {
+            return doSubscribe
+        } else if (count === 0) {
+            return doUnsubscribe
+        } else {
+            return empty();
+        }
+    })
+).subscribe();
+
